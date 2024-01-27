82f9a0f34a9765b9c9abd8de912cfec5e85415ec
liamhebert <liamhebert@gmail.com>
2024-01-27 22:17:34 +0000

    moving files around

--- mDT/experiments/hateful_discussions/download.sh
+++
@@ -1,5 +0,0 @@
-wget https://vault.cs.uwaterloo.ca/s/AMQjNprPxsi4TBr/download/duped-images.parquet
-wget https://vault.cs.uwaterloo.ca/s/ree4cN7ajkoZXQF/download/raw_graphs_image_subset.json 
-wget https://vault.cs.uwaterloo.ca/s/g5KmEicpqEpMrTp/download/image_subset_indexes.tar.gz 
-tar -xvf image_subset_indexes.tar.gz 
-wget https://vault.cs.uwaterloo.ca/s/TfMam3Z2Q93BsML/download/images_bigger.tar.gz 
